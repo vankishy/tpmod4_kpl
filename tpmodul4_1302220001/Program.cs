e79613ec4025db965ed13955fe5e01fe2a8abe65
Reinhard Situmeang <101586289+vankishy@users.noreply.github.com>
2024-03-09 19:11:15 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302220001/Program.cs
+++ tpmodul4_1302220001/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class KodePos
+{
+    public enum EnumKodepos
+    {
+        Batununggal,
+        Kujangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Sekejati,
+        Kebonwaru,
+        Maleer,
+        Samoja
+    }
+
+    public static String getKodePos(EnumKodepos listWilayah)
+    {
+        String[] outputKode = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274", "40273" };
+        return outputKode[(int)listWilayah];
+    }
+
+    public static void Main(String[] args)
+    {
+        EnumKodepos list = EnumKodepos.Sekejati;
+        String kodePos = getKodePos(list);
+        Console.WriteLine("Kode Pos {0} merupakan wilayah {1}", kodePos, list);
+    }
+}
--- tpmodul4_1302220001/obj/Debug/net8.0/tpmodul4_1302220001.AssemblyInfo.cs
+++ tpmodul4_1302220001/obj/Debug/net8.0/tpmodul4_1302220001.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_1302220001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed69103848e689c4774098ca866785952767fa13")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_1302220001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_1302220001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
